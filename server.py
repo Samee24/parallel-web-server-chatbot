a669ee64bfa87d0577c9d469e9e89a9e6fb52321
wheelere <wheelere@grinnell.edu>
2016-04-26 20:24:57 -0500

    fixed chat example to run on localhost:8888

--- server.py
+++ server.py
@@ -1,44 +1,29 @@
-# Basic example from http://www.codestance.com/tutorials-archive/python-tornado-web-server-with-websockets-part-i-441
+# Basic chat implementation from http://code.runnable.com/UqDMKY-VwoAMABDk/simple-websockets-chat-with-tornado-for-python
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-from tornado.options import define, options, parse_command_line
-
-define("port", default=8888, help="run on the given port", type=int)
-
-# we gonna store clients in dictionary..
-clients = dict()
+clients = []
 
 class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        self.write("This is your response")
-        self.finish()
+  @tornado.web.asynchronous
+  def get(request):
+    request.render("index.html")
 
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self, *args):
-        self.id = self.get_argument("Id")
-        self.stream.set_nodelay(True)
-        clients[self.id] = {"id": self.id, "object": self}
+class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
+  def open(self, *args):
+    print("open", "WebSocketChatHandler")
+    clients.append(self)
 
-    def on_message(self, message):        
-        """
-        when we receive some message we want some message handler..
-        for this example i will just print message to console
-        """
-        print "Client %s received a message : %s" % (self.id, message)
+  def on_message(self, message):        
+    print message
+    for client in clients:
+      client.write_message(message)
         
-    def on_close(self):
-        if self.id in clients:
-            del clients[self.id]
+  def on_close(self):
+    clients.remove(self)
 
-app = tornado.web.Application([
-    (r'/', IndexHandler),
-    (r'/', WebSocketHandler),
-])
+app = tornado.web.Application([(r'/chat', WebSocketChatHandler), (r'/', IndexHandler)])
 
-if __name__ == '__main__':
-    parse_command_line()
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+app.listen(8888)
+tornado.ioloop.IOLoop.instance().start()
