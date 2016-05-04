ba5333dd8e0f2fa017c6ced2356acfb068e9198d
Samee24 <sameescouser24@gmail.com>
2016-05-04 08:39:14 -0500

    RayK now reponds to chat messages + use delay

--- alchemy.py
+++ alchemy.py
@@ -2,25 +2,39 @@ from alchemyapi import AlchemyAPI
 import requests
 import json
 alchemyapi = AlchemyAPI()
-
+# Some phrases that will be used by the bot
 GREETING_PROMPT = "Hey there, student!"
 ERROR_PROMPT = "Hey, I couldn't really understand you. Try saying that again."
-print("Hey, I'm Rayk! Your friendly neighborhood president!.")
+# Print opening message
+# print("Hey, I'm Rayk! Your friendly neighborhood president!.")
+# Our set of rules --  a dictionary
 rule_dict = {
     'hello': GREETING_PROMPT,
     'hi': GREETING_PROMPT,
     "what's up": GREETING_PROMPT,
     'hey': GREETING_PROMPT
   }
-while True:
-  myText = raw_input("")
+
+def getRaykResponse(user_input):    # return chat bot response
+  myText = user_input
   response_key = alchemyapi.keywords('text', myText, {'sentiment': 1})
+  # If we got a response back from the API
   if response_key['status'] == 'OK':
+    # Loop over all identified keywords
       for keyword in response_key['keywords']:
         word = keyword['text'].encode('utf-8')
+        # If a rule exists for our word, then use it
         if word.lower() in rule_dict:
-          print(rule_dict[word])
+          return rule_dict[word.lower()]
         else:
-          print(word.lower())
+          print(ERROR_PROMPT)
   else:
       print('Error in keyword extaction call: ', response_key['statusInfo'])
+
+def calculateDelay(response):  # return delay in seconds for each reponse using WPM = 90
+    delay = len(response.split())/1.5
+    if (delay > 4):
+      delay = 4
+  return delay
+
+
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import json
+import alchemy
+import time
 
 clients = []
 
@@ -24,28 +26,39 @@ class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
   getting a response from the chatbot, and returning both the message and its
   response to the client (separately).
   '''
-  def on_message(self, message):        
+  def on_message(self, message):
     dict = json.loads(message)
     print message
     # handle different types of messages
     if dict["type"] == "username":
-      self.load_user( dict )
+      self.load_user(dict)
     elif dict["type"] == "message":
-      self.process_chat( dict )
-    
-    
-        
+      self.process_chat(dict)
+
+
+
   def load_user(self, message_dict ):
     d = { 'user': '[SYSTEM]',
           'message': "Login attempt by %s." % (message_dict["user"])}
-    self.write_message( json.dumps(d) )
+    self.write_message(json.dumps(d))
     # TODO: load or create user data
     return
 
-  def process_chat( self, message_dict ):
+  def process_chat(self, message_dict ):
     message_dict.pop( "type", None )
     self.write_message(json.dumps(message_dict))
-    # TODO: Get response from the chatbot and return it
+    # fetch reponse from bot
+    bot_response = alchemy.getRaykResponse(message_dict["message"])
+    # create message dict
+    d = { 'user': 'RayK',
+          'message': "%s." % bot_response}
+    print bot_response
+    # calculate delay using WPM = 90
+    delay = alchemy.calculateDelay(bot_response)
+    # sleep for the length of our delay (which is in seconds)
+    time.sleep(alchemy.calculateDelay(bot_response))
+    # send response to client
+    self.write_message(json.dumps(d))
 
   def on_close(self):
     clients.remove(self)
